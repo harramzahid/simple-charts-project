d14c7249a41eb72ed96ca97ce9a484d49663e8af
Harram Zahid <harramzahid5@gmail.com>
2024-12-02 17:52:02 -0500

    Update script.js

--- script.js
+++ script.js
@@ -4,10 +4,13 @@ new Chart(ctx, {
     type: 'bar',
     data: {
       labels: ['Strawberry Matcha', 'Ube Matcha Latte', 'Matcha Affogato'],
-      datasets: [{
+      datasets: [
+        {
         label: '# of Orders',
         data: [12, 19, 3],
-        borderWidth: 1
+        backgroundColor: ["pink", "grey", "green"],
+        borderColor: ["darkpink", "darkgrey", "darkgreen"],
+        borderWidth: 2
       }]
     },
   });
